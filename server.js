889df50dc806dd1ce09a6bacf2b7d2f428e84c4f
akanksha089 <achaturvdi089@gmail.com>
2025-03-03 15:39:51 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
+console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // Debugging line
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const authRoutes = require("./routes/authRoutes");
 const courseRoutes = require("./routes/courseRoutes");
+
 const app = express();
 
 // Middleware
@@ -11,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
